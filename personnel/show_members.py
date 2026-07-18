c9f65301bf3e1bd5dd2277c39694c32b14ef2521
Kamjondet <98246079+CatDBB101@users.noreply.github.com>
2026-07-18 11:09:17 +0700

    feat(personnel/show_members): show_members function

--- personnel/show_members.py
+++ personnel/show_members.py
@@ -5,7 +5,12 @@ from data import family_members
 
 # TODO: สร้างฟังก์ชัน show_members()
 #   - print ข้อมูลลูกน้องทุกคนใน family_members บรรทัดละคน (ชื่อ, ตำแหน่ง, ความโหด, อาวุธ)
-
+def show_members():
+    print("Member List")
+    print("=" * 50)
+    for member in family_members:
+        print(f"{member["name"]}({member["role"]}) - age:{member["age"]} power:{member["power"]} money:{member["money"]} equipment:{member["equipment"]}")
+    print("=" * 50)
 
 # ทดสอบ: python -m personnel.show_members
 if __name__ == "__main__":
