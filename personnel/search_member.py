76c5187d6bb2b8e921cf69184efa5050ae7a9a74
Student <Student@Train11-5-PC127.control.ad.sit.kmutt.ac.th>
2026-07-18 11:33:59 +0700

    Search success

--- personnel/search_member.py
+++ personnel/search_member.py
@@ -1,5 +1,5 @@
 # =====================================================
-#  personnel/search_member.py — คนรับผิดชอบ: ______________________
+#  personnel/search_member.py — คนรับผิดชอบ: ราชาแอ็กเกอร์ตัวปลอม
 # =====================================================
 from data import family_members
 
@@ -7,8 +7,19 @@ from data import family_members
 #   - หาคนใน family_members ที่ชื่อตรงกับ target_name (ไม่สนตัวพิมพ์ใหญ่/เล็ก)
 #   - เจอ -> return dict ของคนนั้น | ไม่เจอ -> return None
 
-
-# ทดสอบ: python -m personnel.search_member
+def search_member (name) :
+    result = None
+    for member in family_members:
+        if member["name"].lower() == name.lower():
+            result = member
+        else : 
+            result == "ไม่พบสมาชิค"
+    return result
+            
 if __name__ == "__main__":
-    print(search_member("tony"))        # ต้องได้ dict ของ Tony (พิมพ์เล็กก็ต้องเจอ)
-    print(search_member("ไม่มีคนนี้"))    # ต้องได้ None
+    print(search_member("tony"))
+
+
+# ต้องได้ dict ของ Tony (พิมพ์เล็กก็ต้องเจอ)
+# ทดสอบ: python -m personnel.search_member
+# ต้องได้ None
