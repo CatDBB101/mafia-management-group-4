bb3ed058fce7bbe2075516cc8a1d837791ef9c2e
greenbank <s.jitsopin@gmail.com>
2026-07-18 11:47:49 +0700

    show_catalog.py

--- weapon_shop/show_catalog.py
+++ weapon_shop/show_catalog.py
@@ -1,12 +1,20 @@
 # =====================================================
-#  weapon_shop/show_catalog.py — คนรับผิดชอบ: ______________________
+#  weapon_shop/show_catalog.py — คนรับผิดชอบ: แบงค์
 # =====================================================
 from data import weapons_catalog
 
 # TODO: สร้างฟังก์ชัน show_catalog()
 #   - print อาวุธทุกชิ้นใน weapons_catalog บรรทัดละชิ้น (รหัส, ชื่อ, ราคา, พลังโบนัส)
-
+def show_catalog():
+ for num in weapons_catalog:
+  
+  weapons = weapons_catalog[num]
+  print(f"{weapons["name"]},{weapons["bonus"]},{weapons["price"]}")
 
 # ทดสอบ: python -m weapon_shop.show_catalog
 if __name__ == "__main__":
-    show_catalog()   # ต้องเห็นอาวุธครบ 3 ชิ้น
+ show_catalog()   # ต้องเห็นอาวุธครบ 3 ชิ้น
+
+
+
+
